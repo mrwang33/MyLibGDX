bb9f2825de90a16f9614c23626ee18957b06a3ad
wang <wang1386528@gmail.com>
2017-09-24 22:50:01 +0800

    annotation

--- HelloLibGDX/core/src/com/mygdx/game/MyGdxGame.java
+++ HelloLibGDX/core/src/com/mygdx/game/MyGdxGame.java
@@ -7,14 +7,16 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyGdxGame extends ApplicationAdapter {
+	//画布
 	SpriteBatch batch;
-	//Texture img;
+	//纹理
+	Texture img;
 	Texture acfun;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		//img = new Texture("badlogic.jpg");
+		img = new Texture("badlogic.jpg");
 		acfun = new Texture("ac1-1.gif");
 	}
 
@@ -23,7 +25,7 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		//batch.draw(img, 0, 0);
+		batch.draw(img, 0, 0);
 		batch.draw(acfun,40,40);
 		batch.end();
 	}
