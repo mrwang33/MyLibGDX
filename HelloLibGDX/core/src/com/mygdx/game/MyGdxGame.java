3a8dffa52babe56d8a4b07656ae639f8547de2a5
wang <wang1386528@gmail.com>
2017-09-24 22:46:37 +0800

    libgdx init

---
+++ HelloLibGDX/core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,30 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MyGdxGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	//Texture img;
+	Texture acfun;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		//img = new Texture("badlogic.jpg");
+		acfun = new Texture("ac1-1.gif");
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		//batch.draw(img, 0, 0);
+		batch.draw(acfun,40,40);
+		batch.end();
+	}
+}
