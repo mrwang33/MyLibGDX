665fa96e54342574845f8d698a7b1d32e8171e82
wang <wang1386528@gmail.com>
2017-10-09 23:27:17 +0800

    actor group

--- HelloLibGDX/core/src/com/mygdx/game/MyGdxGame.java
+++ HelloLibGDX/core/src/com/mygdx/game/MyGdxGame.java
@@ -6,15 +6,21 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.mygdx.actor.MyActor;
-import com.mygdx.input.MyInputListener;
 
 public class MyGdxGame extends ApplicationAdapter {
 	//画布
 	private SpriteBatch batch;
 	//纹理
-	private Texture acfun;
+	private Texture texture;
+	private Group group;
+
+	private MyActor actor1;
+
+	private MyActor actor2;
+
 	//演员
 	private MyActor actor;
 	//舞台
@@ -22,25 +28,56 @@ public class MyGdxGame extends ApplicationAdapter {
 
 	@Override
 	public void create () {
-		acfun = new Texture(Gdx.files.internal("badlogic.jpg"));
-		actor = new MyActor(new TextureRegion(acfun));
-		 /* 设置演员属性的值 */
-		// 设置演员的坐标
-		actor.setPosition(50, 100);     // 或者 setX(), setY() 分开设置
+		// 创建纹理, badlogic.jpg 图片的宽高为 256 * 256
+		texture = new Texture(Gdx.files.internal("badlogic.jpg"));
 
-		// 设置演员 旋转和缩放支点 为演员的左下角
-		actor.setOrigin(0, 0);
-
-		// 设置演员缩放比, X 轴方向缩小到 0.5 倍, Y 轴方向保持不变
-		actor.setScale(0.5F, 1.0F);
-		// 顺时针旋转 5 度
-		actor.setRotation(-5);
-		//初始化舞台
+		// 创建舞台, 舞台宽高默认为屏幕宽高
 		stage = new Stage();
-		stage.addActor(actor);
-		//注册按键监听器 将舞台交给他
-		Gdx.input.setInputProcessor(stage);
-		stage.addListener(new MyInputListener());
+
+		// 创建演员组
+		group = new Group();
+		// 设置 group 在舞台中的位置
+		group.setPosition(50, 100);
+
+		// 添加 group 到舞台（group 本身也是一个演员)
+		stage.addActor(group);
+
+		// 创建第一个演员, 并设置属性
+		actor1 = new MyActor(new TextureRegion(texture));
+		actor1.setScale(0.5F);
+		actor1.setPosition(0, 0);   // 位置设置到 group 的左下角
+
+		// 创建第二个演员, 并设置属性
+		actor2 = new MyActor(new TextureRegion(texture));
+		actor2.setScale(0.25F);
+		actor2.setPosition(100, 200);
+		actor2.setRotation(45);     // 逆时针旋转 45 度
+
+		// 添加演员到组中
+		group.addActor(actor1);
+		group.addActor(actor2);
+
+		// 还可以设置演员的绘制顺序（ZIndex 属性）
+
+        /*
+         * ZIndex 属性说明:
+         *
+         * 当组中有许多演员时, 如果某些演员有重叠部分, 往往需要规定演员的绘制顺序, 例如背景先绘制,
+         * 人物后绘制, 在 Actor 中使用 ZIndex 属性来表示绘制顺序, ZIndex 值越大越后绘制, 即显示越靠前。
+         *
+         * ZIndex 属性值其实对应的是 Group 中的演员数组对象中演员所在的索引位置（添加到 Group 中的演员的默认添加到数组的最后位置）。
+         * 绘制时 Group 将遍历数组依次绘制演员, 则越靠后（ZIndex/index 越大）的元素越后绘制（即显示越靠前）。
+         *
+         * 因此, 必须先将 Actor 添加到 Group（或 Stage, 因为 Stage 也是通过 Group 管理其中的演员）中之后才能操作 ZIndex 属性,
+         * 否则 ZIndex 属性没有意义。
+         *
+         * // 获取演员的索引, 如果 actor 没有添加到 Group/Stage 中, 将返回 -1
+         * int index = actor.getZIndex();
+         *
+         * // 设置演员的索引（实际上是将数组中的演员元素移动到指定的索引位置, 可能会引起其他演员的 ZIndex 值跟着改变）
+         * // 如果 actor 没有添加到 Group/Stage 中, 设置 ZIndex 属性无效
+         * actor.setZIndex(int index);
+         */
 	}
 
 	@Override
@@ -48,21 +85,19 @@ public class MyGdxGame extends ApplicationAdapter {
 		// 黑色清屏
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		// 更新演员逻辑
-		actor.act(Gdx.graphics.getDeltaTime());
-		//批处理调用每个演员的act
+
+		// 更新舞台逻辑
 		stage.act();
-		//绘制舞台
+		// 绘制舞台
 		stage.draw();
 	}
 
 	@Override
 	public void dispose() {
-		// 应用退出, 纹理不在需要用到, 释放纹理资源
-		if (acfun != null) {
-			acfun.dispose();
+		// 释放资源
+		if (texture != null) {
+			texture.dispose();
 		}
-		// 当应用退出时释放资源
 		if (stage != null) {
 			stage.dispose();
 		}
